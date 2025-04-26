09433da12641e9c29c5574a385d4d37185445c4f
Gustavias <160853754+Gustavias@users.noreply.github.com>
2025-04-26 09:26:54 +0200

    Navigation sub

---
+++ data.js
@@ -0,0 +1,397 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_001-dock",
+      "name": "IMG_001 Dock",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -2.7368328304234577,
+        "pitch": -0.28181579314086136,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_002-torpedo-room",
+      "name": "IMG_002 Torpedo Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_004-fwd-battery-and-crew-station",
+      "name": "IMG_004 FWD Battery and Crew Station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -0.8838189858783245,
+        "pitch": 0.019926369211976436,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-img_007-officers-wardroom",
+      "name": "IMG_007 Officers Wardroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_009-bridge",
+      "name": "IMG_009 Bridge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-img_011-bridge-periscope",
+      "name": "IMG_011 Bridge Periscope",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": 1.2538812784064461,
+        "pitch": 0.05408343229813184,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-img_012-ops-room",
+      "name": "IMG_012 Ops Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-img_013-ops-room-aft",
+      "name": "IMG_013 Ops Room Aft",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -0.7224088370096773,
+        "pitch": -0.045545986770239466,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-img_016-engine-room",
+      "name": "IMG_016 Engine Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -0.9172822650555759,
+        "pitch": 0.0540858592896587,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-img_018-galley",
+      "name": "IMG_018 Galley",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": 2.470363711915276,
+        "pitch": 0.11956090505022132,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-img_019-generator",
+      "name": "IMG_019 Generator",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -2.312580318643235,
+        "pitch": -0.005657563383653752,
+        "fov": 1.211801309678363
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-img_021-generator-control-panel",
+      "name": "IMG_021 Generator Control Panel",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": -1.3286811034919168,
+        "pitch": -0.1053250944061741,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-img_028-conning-tower",
+      "name": "IMG_028. Conning Tower",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 760,
+      "initialViewParameters": {
+        "yaw": 1.7341748921131526,
+        "pitch": -0.9820853397333167,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-img_033_pureshot",
+      "name": "IMG_033_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.3799043747761992,
+        "pitch": 0.008539872519410352,
+        "fov": 1.2126618977576595
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360 Pano Images",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
